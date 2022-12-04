aea1c030705c4eb7754d5666fcd8ba142ac24cad
Полина Гавриленко <79043262656@ya.ru>
2022-12-04 11:17:57 +0600

    add working version without classes

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from random import randint
 
 
+
+from graphic_arts.start_game_banner import run_screensaver
+
+
 def attack(char_name: str, char_class: str) -> str:
+    """Генерирует количество очков атаки."""
     if char_class == 'warrior':
         return (
             f'{char_name} нанёс урон противнику равный {5 + randint(3, 5)}')
@@ -14,6 +19,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Генерирует количество очков защиты."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -23,6 +29,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Возвращает сообщение о применении специального умения."""
     if char_class == 'warrior':
         return
         (f'{char_name} применил специальное умение «Выносливость {80 + 25}»')
@@ -33,6 +40,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Возвращает сообщение о применении специального умения."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -58,6 +66,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Возвращает тип игрового персонажа, выбранного игроком."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -83,7 +92,9 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> str:
+if __name__ == '__main__':
+    """Запускает игру."""
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
